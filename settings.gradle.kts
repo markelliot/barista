9640eba9d30bef0b7a574792eeb5612595b12e22
Mark Elliot <mark.elliot@gmail.com>
2021-11-17 17:25:07 -0500

    Use Gradle plugins repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,23 +2,3 @@ rootProject.name = "barista-root"
 
 include("barista")
 
-pluginManagement {
-    repositories {
-        maven {
-            url = uri("https://maven.pkg.github.com/markelliot/gradle-versions")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GH_READ_PACKAGES_TOKEN")
-            }
-        }
-        gradlePluginPortal()
-    }
-
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.id == "com.markelliot.versions") {
-                 useModule("com.markelliot.gradle.versions:gradle-versions:${requested.version}")
-            }
-        }
-    }
-}
