57c6b36aee1d3bf1a28597324e122f109a542115
Mark Elliot <mark.elliot@gmail.com>
2022-02-10 13:44:43 +0000

    Bump com.markelliot.versions manually (#38)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.diffplug.spotless") version "6.2.2"
     id("com.google.cloud.tools.jib") version "3.2.0" apply false
     id("com.palantir.consistent-versions") version "2.7.0"
-    id("com.markelliot.versions") version "0.1.12"
+    id("com.markelliot.versions") version "0.2.0"
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     id("net.ltgt.errorprone") version "2.0.2" apply false
     id("org.inferred.processors") version "3.6.0" apply false
