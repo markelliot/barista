3ae58c15cbd3b4634ca4b6cd6ea16c526b2104b4
Mark Elliot <mark.elliot@gmail.com>
2022-03-02 15:42:57 -0500

    format

--- barista-conjure/src/main/java/com/markelliot/barista/conjure/ConjureAdapter.java
+++ barista-conjure/src/main/java/com/markelliot/barista/conjure/ConjureAdapter.java
@@ -1,3 +1,19 @@
+/*
+ * (c) Copyright 2022 Mark Elliot. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.markelliot.barista.conjure;
 
 import com.markelliot.barista.HttpMethod;
@@ -15,16 +31,15 @@ import java.util.stream.Collectors;
  * Adapts Conjure endpoints for use with Barista.
  *
  * <p>Example usage:
- * <pre>
- * {@code
+ *
+ * <pre>{@code
  * UndertowRuntime conjureRuntime = ConjureUndertowRuntime.builder().build();
  * Endpoints endpoints = ConjureAdapter.adapt(
  *         SampleServiceEndpoints.of(new DefaultSampleService()), conjureRuntime);
  * Server.builder()
  *         .endpoints(endpoints)
  *         ...
- * }
- * </pre>
+ * }</pre>
  */
 public final class ConjureAdapter {
     private ConjureAdapter() {}
