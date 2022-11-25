4e0fda726fba6ae9bd5f3cbe7fbd4eed83ae1bc6
mellbot <94683073+mellbot@users.noreply.github.com>
2022-11-24 20:15:03 -0500

    Auto-update dependencies and plugins (#152)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import net.ltgt.gradle.errorprone.errorprone
 
 plugins {
     idea
-    id("com.diffplug.spotless") version "6.11.0"
+    id("com.diffplug.spotless") version "6.12.0"
     id("com.google.cloud.tools.jib") version "3.3.1" apply false
     id("com.palantir.consistent-versions") version "2.11.0"
     id("com.markelliot.versions") version "0.24.0"
