c7a72e7fa7afb2b2138d82d410044ad85943a495
Felipe Orozco <ferozco@users.noreply.github.com>
2023-10-10 18:27:00 -0400

    Expose fallback callback (#262)
    ## Before this PR
    It wasn't possible to take any action or collect any metrics when a request did not match any handler
    
    ## After this PR
    Expose fallback callback
    
    Consumers of this library can register a callback to be called with a read-only view of the request to take what action they would like
    
    ## Possible downsides?
    <!-- Please describe any way users could be negatively affected by this PR. -->

--- barista/src/main/java/com/markelliot/barista/handlers/EndpointHandlerBuilder.java
+++ barista/src/main/java/com/markelliot/barista/handlers/EndpointHandlerBuilder.java
@@ -16,19 +16,29 @@
 
 package com.markelliot.barista.handlers;
 
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.Multimaps;
+import com.markelliot.barista.Request;
 import com.markelliot.barista.SerDe;
 import com.markelliot.barista.authz.Authz;
 import com.markelliot.barista.endpoints.EndpointHandler;
 import com.markelliot.barista.endpoints.EndpointRuntime;
 import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
 import io.undertow.server.RoutingHandler;
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.Set;
+import java.util.TreeMap;
+import java.util.function.Consumer;
 
 public final class EndpointHandlerBuilder {
     private final SerDe serde;
     private final Authz authz;
+    private final Optional<Consumer<Request>> fallbackHandler;
 
-    public EndpointHandlerBuilder(SerDe serde, Authz authz) {
+    public EndpointHandlerBuilder(SerDe serde, Authz authz, Optional<Consumer<Request>> fallbackHandler) {
+        this.fallbackHandler = fallbackHandler;
         this.serde = serde;
         this.authz = authz;
     }
@@ -37,8 +47,24 @@ public final class EndpointHandlerBuilder {
         EndpointRuntime runtime = new EndpointRuntime(serde, authz);
         RoutingHandler router = new RoutingHandler(false);
         endpointHandlers.forEach(e -> router.add(e.method().method(), e.route(), e.handler(runtime)));
-        router.setFallbackHandler(
-                exchange -> exchange.setStatusCode(404).getResponseSender().send("Unknown API Endpoint"));
+        router.setFallbackHandler(exchange -> {
+            fallbackHandler.ifPresent(requestConsumer -> requestConsumer.accept(toRequest(exchange)));
+            exchange.setStatusCode(404).getResponseSender().send("Unknown API Endpoint");
+        });
         return router;
+    }
+
+    private static Request toRequest(HttpServerExchange exchange) {
+        ListMultimap<String, String> headers =
+                Multimaps.newListMultimap(new TreeMap<>(String.CASE_INSENSITIVE_ORDER), ArrayList::new);
+        ListMultimap<String, String> queryParams =
+                Multimaps.newListMultimap(new TreeMap<>(String.CASE_INSENSITIVE_ORDER), ArrayList::new);
+
+        exchange.getQueryParameters().forEach(queryParams::putAll);
+        exchange.getRequestHeaders().forEach(headerValue -> {
+            headers.putAll(headerValue.getHeaderName().toString(), headerValue);
+        });
+
+        return new Request(exchange.getRequestPath(), headers, queryParams);
     }
 }
