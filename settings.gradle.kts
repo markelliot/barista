600836edb1a0ab0ae69198ef1f87ade97b05b435
Mark Elliot <markelliot@users.noreply.github.com>
2025-04-29 16:00:53 -0400

    Pin Java version to 17 using Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,10 @@
 rootProject.name = "barista-root"
 
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
+
 include("barista")
 include("barista-annotations")
 include("barista-conjure")
