91ab5d2a7f313f6eadf775c0e34ec3fa56aab9ea
Mark Elliot <markelliot@users.noreply.github.com>
2025-09-30 11:58:26 -0400

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     id("com.diffplug.spotless") version "8.0.0"
     id("com.google.cloud.tools.jib") version "3.4.5" apply false
     id("com.palantir.consistent-versions") version "3.2.0"
-    id("com.markelliot.versions") version "0.133.0"
     id("net.ltgt.errorprone") version "4.3.0" apply false
     id("org.inferred.processors") version "3.7.0" apply false
     id("org.jreleaser") version "1.20.0"
@@ -138,8 +137,4 @@ jreleaser {
             }
         }
     }
-}
-
-tasks.updateGradleWrapper {
-    enabled = false
 }
